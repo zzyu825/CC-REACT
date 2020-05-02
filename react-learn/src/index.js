15b4a6820a2159df821a2f28fa095bd387b99c93
zhengzhiyu <zhengzhiyu825@163.com>
2020-05-02 18:15:21 +0800

    2-2

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import src1 from './assets/1.jpg';
+import src2 from './assets/2.jpg';
+import src3 from './assets/3.jpg';
+import './index.css';
 
-let num = 1;
+let index = 0; // 显示图片的索引
+const srcs = [src1, src2, src3]; // 保存图片路径的数组
+const root = document.getElementById('root');
+let timer; // 计时器
 
-setInterval(() => {
-  num ++;
-  const div = (
-    <div title="标题">
-      {num}
-    </div>
-  );
-  ReactDOM.render(div, document.getElementById('root'));
-}, 1000);
+/**
+ * 根据index的值，渲染图片
+ */
+function render() {
+    ReactDOM.render(<img src={srcs[index]} />, root);
+}
 
-// const div = (
-//   <div title="标题">
-//     {num}
-//   </div>
-// );
+/**
+ * 启动计时器，每隔一段时间切换图片
+ */
+function start() {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        index = (index + 1) % 3;
+        render();
+    }, 2000)
+}
 
-// console.log(div);
+/**
+ * 停止计时器
+ */
+function stop() {
+    clearInterval(timer);
+}
 
-// div.props.children = 2;
-// div.props.title = '标题2';
+render();
 
-// ReactDOM.render(div, document.getElementById('root'));
+start();
+
+root.onmouseenter = () => {
+    stop();
+}
+
+root.onmouseleave = () => {
+    start();
+}
