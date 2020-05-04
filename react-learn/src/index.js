8ae20eaf58b0f5253bd09d976991e93c745449d0
zhengzhiyu <zhengzhiyu825@163.com>
2020-05-04 16:08:26 +0800

    2-6

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Tick from './components/Tick';
-import Test from './components/Test';
+import Ball from './components/Ball';
+import BallList from './components/BallList';
 
-// let num = 10,
-//     timer = null;
-/**
- * 倒计时函数
- */
-// function tick() {
-//     timer = setInterval(() => {
-//         num--;
-//         if (num <= 0) {
-//             clearInterval(timer);
-//         }
-//         ReactDOM.render(<Tick number={num} />, document.getElementById('root'));
-//     }, 1000);
-// }
-
-// tick();
-
-// ReactDOM.render(<Tick number={10} />, document.getElementById('root'));
-ReactDOM.render(<Test />, document.getElementById('root'));
+// ReactDOM.render(<Ball xSpeed={300} ySpeed={200} left={100} top={100} bg={"#ff0"}/>, document.getElementById('root'));
+ReactDOM.render(<BallList />, document.getElementById('root'));
