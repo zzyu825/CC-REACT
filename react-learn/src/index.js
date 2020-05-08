913df7a8982cb8a933c58eba4faa7ceeafb34ec1
zhengzhiyu <zhengzhiyu825@163.com>
2020-05-08 17:23:34 +0800

    2-10

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Comp from './components/Comp';
+import App from './components/App';
 
-ReactDOM.render(<Comp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
