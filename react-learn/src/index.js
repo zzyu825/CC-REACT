b164d184abcc9518598354eb3f111481cb40f1b0
zhengzhiyu <zhengzhiyu825@163.com>
2020-05-08 18:16:28 +0800

    2-11

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Comp from './Comp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    // <Comp children={<h2>传递的元素内容</h2>}/>
+    // <Comp>
+    //     <h2>传递的元素内容</h2>
+    // </Comp>
+    <Comp content1={<h2>元素内容2</h2>} content2={<h2>元素内容3</h2>}>
+        <h2>元素内容1</h2>
+    </Comp>
+), document.getElementById('root'));
 
