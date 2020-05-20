3d84f82ae36a8f6cbc71de8153881459e6dae18c
zhengzhiyu <zhengzhiyu825@163.com>
2020-05-20 17:02:41 +0800

    3-7

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Comp from './components/Comp'
+import Test from './components/common/Banner/Test'
 export default class App extends Component {
 
     render() {
         return (
             <>
-                <Comp />
+                <Test />
             </>
         )
     }
