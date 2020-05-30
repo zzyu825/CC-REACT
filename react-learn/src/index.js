eee02820a4a0f2f42f59396dfce214e9b41e3463
zhengzhiyu <zhengzhiyu825@163.com>
2020-05-31 06:30:37 +0800

    3-15

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-document.addEventListener("click", e => {
-    console.log("真实dom：document被点击了")
-})
+ReactDOM.render(<App/>, document.getElementById('root'));
