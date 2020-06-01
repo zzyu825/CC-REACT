820d50963902935a2bc2a146355cfa34473a2039
zhengzhiyu <zhengzhiyu825@163.com>
2020-06-01 19:39:22 +0800

    4-4

--- react-learn/src/services/student.js
+++ react-learn/src/services/student.js
@@ -4,6 +4,14 @@ const appkey = "zzyu_1564402433694";
  * 获取所有学生
  */
 export async function getStudents() {
-    return await fetch(`http://api.duyiedu.com/api/student/findAll?appkey=${appkey}`)
+    return await fetch(`/api/student/findAll?appkey=${appkey}`)
+        .then(resp => resp.json() ).then(resp => resp.data );
+}
+
+/**
+ * 通过分页获取学生
+ */
+export async function getStudent(page = 1, limit = 10) {
+    return await fetch(`/api/student/findByPage?appkey=${appkey}&page=${page}&size=${limit}`)
         .then(resp => resp.json() ).then(resp => resp.data );
 }
