c3628d4f861459382b02856e06531d4fd42ad83f
zhengzhiyu <zhengzhiyu825@163.com>
2020-06-04 08:52:52 +0800

    4-6

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
-import useTimer from "./myHooks/useTimer"
+// import React, { useReducer } from 'react'
 
-function Test() {
-    useTimer(() => {
-        console.log("Test组件的一些副作用操作")
-    }, 1000);
-    return <h1>Test组件</h1>
+import React from "react"
+import useReducer from "./useReducer"
+
+/**
+ * 该函数，根据当前的数据，已经action，生成一个新的数据
+ * @param {*} state 
+ * @param {*} action 
+ */
+function reducer(state, action) {
+    switch (action.type) {
+        case "increase":
+            return state + 1;
+        case "decrease":
+            if (state === 0) {
+                return 0;
+            }
+            return state - 1;
+        default:
+            return state;
+    }
 }
 
 export default function App() {
-    const [visible, setVisible] = useState(true)
+    const [n, dispatch] = useReducer(reducer, 10, (args) => {
+        console.log(args)
+        return 100
+    });
     return (
         <div>
-            {
-                visible && <Test />
-            }
             <button onClick={() => {
-                setVisible(!visible)
-            }}>显示/隐藏</button>
+                dispatch({ type: "decrease" })
+            }}>-</button>
+            <span>{n}</span>
+            <button onClick={() => {
+                dispatch({ type: "increase" })
+            }}>+</button>
         </div>
     )
 }
