ba5f442c6557d45d746a7b2da221f639a8ca4c97
zhengzhiyu <zhengzhiyu825@163.com>
2020-06-07 15:01:56 +0800

    4-7

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,41 +1,24 @@
-// import React, { useReducer } from 'react'
+import React, { useContext } from "react"
 
-import React from "react"
-import useReducer from "./useReducer"
+const ctx = React.createContext();
 
-/**
- * 该函数，根据当前的数据，已经action，生成一个新的数据
- * @param {*} state 
- * @param {*} action 
- */
-function reducer(state, action) {
-    switch (action.type) {
-        case "increase":
-            return state + 1;
-        case "decrease":
-            if (state === 0) {
-                return 0;
-            }
-            return state - 1;
-        default:
-            return state;
-    }
+// function Test() {
+//     return <ctx.Consumer>
+//         {val => <h1>上下文的值：{val}</h1>}
+//     </ctx.Consumer>
+// }
+
+function Test() {
+    const val = useContext(ctx);
+    return <h1>上下文的值：{val}</h1>
 }
 
 export default function App() {
-    const [n, dispatch] = useReducer(reducer, 10, (args) => {
-        console.log(args)
-        return 100
-    });
     return (
         <div>
-            <button onClick={() => {
-                dispatch({ type: "decrease" })
-            }}>-</button>
-            <span>{n}</span>
-            <button onClick={() => {
-                dispatch({ type: "increase" })
-            }}>+</button>
+            <ctx.Provider value="abc">
+                <Test />
+            </ctx.Provider>
         </div>
     )
 }
