c650bb4b80ebfeeb8a6fd4d6757ff56ac34a0505
zhengzhiyu <zhengzhiyu825@163.com>
2020-06-17 18:47:09 +0800

    5-8

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,56 +1,43 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, NavLink, Switch, Redirect } from "react-router-dom"
-import "./App.css";
-// import Link from "./Link"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import  routeConfig from "./RouteConfig"
 
-function PageA() {
-    return <h1>A页</h1>
+function User({match}) {
+    console.log(match);
+    return (
+        <div>
+            <h1>User组件固定的区域</h1>
+            <p>
+                <Link to={routeConfig.user.update}>用户信息</Link>
+                <Link to={routeConfig.user.pay.root}>充值</Link>
+            </p>
+            <div style={{
+                width: 500,
+                height: 500,
+                background: "lightblue",
+                border: "2px solid",
+                margin: "0 auto"
+            }}>
+                <Route path={routeConfig.user.update} component={UserUpdate} />
+                <Route path={routeConfig.user.pay.root} component={UserPay} />
+            </div>
+        </div>
+    )
 }
 
-function PageB() {
-    return <h1>B页</h1>
+function UserUpdate() {
+    return <h1>修改用户信息</h1>
 }
 
-function NavBar() {
-    return <div>
-        <NavLink activeClassName="selected"
-            exact
-            strict
-            activeStyle={{
-                background: "#ccc"
-            }}
-            innerRef={node => {
-                console.log(node)
-            }} to="/a" style={{ marginRight: 20 }}>去a页</NavLink>
-        <NavLink activeClassName="selected"
-            activeStyle={{
-                background: "#ccc"
-            }}
-            replace to={{
-                pathname: "/b",
-                hash: "#abc",
-                search: "?a=1&b=2"
-            }}>去b页</NavLink>
-
-        <NavLink
-            to="/abc"
-            style={{ marginLeft: 20 }}>
-            其他页
-        </NavLink>
-    </div>
+function UserPay() {
+    return <h1>用户充值</h1>
 }
 
 export default function App() {
     return (
         <Router>
-            <NavBar />
-            <Switch>
-                <Route path="/a" component={PageA} />
-                <Route path="/b" component={PageB} />
-                <Redirect
-                from="/abc/:id"
-                to="/a/:id" />
-            </Switch>
+            <Route path="/user" component={User} />
+            {/* 其他组件 */}
         </Router>
     )
 }
