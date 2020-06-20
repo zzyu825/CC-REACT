205446d7efe830357c5ca2154839beb330994c89
zhengzhiyu <zhengzhiyu825@163.com>
2020-06-21 07:55:48 +0800

    5-9

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,43 +1,32 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import  routeConfig from "./RouteConfig"
-
-function User({match}) {
-    console.log(match);
-    return (
-        <div>
-            <h1>User组件固定的区域</h1>
-            <p>
-                <Link to={routeConfig.user.update}>用户信息</Link>
-                <Link to={routeConfig.user.pay.root}>充值</Link>
-            </p>
-            <div style={{
-                width: 500,
-                height: 500,
-                background: "lightblue",
-                border: "2px solid",
-                margin: "0 auto"
-            }}>
-                <Route path={routeConfig.user.update} component={UserUpdate} />
-                <Route path={routeConfig.user.pay.root} component={UserPay} />
-            </div>
-        </div>
-    )
-}
-
-function UserUpdate() {
-    return <h1>修改用户信息</h1>
-}
-
-function UserPay() {
-    return <h1>用户充值</h1>
-}
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom"
+import Home from "./Home"
+import Personal from "./Personal"
+import Login from "./Login"
+import ProtectedRoute from "./ProtectedRoute"
 
 export default function App() {
     return (
         <Router>
-            <Route path="/user" component={User} />
-            {/* 其他组件 */}
+            <div>
+                <ul>
+                    <li><Link to="/">首页</Link></li>
+                    <li><Link to="/login">登录页</Link></li>
+                    <li><Link to="/personal">个人中心</Link></li>
+                </ul>
+                <div>
+                    <Switch>
+                        <Route path="/login" component={Login} />
+                        <ProtectedRoute path="/personal" component={Personal}/>
+                        {/* render和children的区别：render是匹配后才会运行，children无论是否匹配都会运行 */}
+                        {/* <Route path="/personal" render={ values => {
+                            console.log(values)
+                            return <h1>asdfdasdfa</h1>
+                        }} /> */}
+                        <Route path="/" component={Home} />
+                    </Switch>
+                </div>
+            </div>
         </Router>
     )
 }
