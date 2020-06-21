fbba518118d434ef1906b08e7e6d424ac58b33cb
zhengzhiyu <zhengzhiyu825@163.com>
2020-06-21 08:00:27 +0800

    5-10

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,31 +1,19 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch, Link } from "react-router-dom"
-import Home from "./Home"
-import Personal from "./Personal"
-import Login from "./Login"
-import ProtectedRoute from "./ProtectedRoute"
+import { BrowserRouter as Router } from "react-router-dom"
+import Link from "./BetterLink"
+import RootRouter from "./RootRouter";
+import "./App.css"
 
 export default function App() {
     return (
         <Router>
+            <nav>
+                <Link to={{name:"home"}}>首页</Link>
+                <Link to={{name:"news"}}>新闻页</Link>
+            </nav>
             <div>
-                <ul>
-                    <li><Link to="/">首页</Link></li>
-                    <li><Link to="/login">登录页</Link></li>
-                    <li><Link to="/personal">个人中心</Link></li>
-                </ul>
-                <div>
-                    <Switch>
-                        <Route path="/login" component={Login} />
-                        <ProtectedRoute path="/personal" component={Personal}/>
-                        {/* render和children的区别：render是匹配后才会运行，children无论是否匹配都会运行 */}
-                        {/* <Route path="/personal" render={ values => {
-                            console.log(values)
-                            return <h1>asdfdasdfa</h1>
-                        }} /> */}
-                        <Route path="/" component={Home} />
-                    </Switch>
-                </div>
+                {/* 匹配网站的顶级页面 */}
+                <RootRouter />
             </div>
         </Router>
     )
